f28dcbd0ee4c0bb0eec9052950cbffb160b6c770
Samruddhi Bhabad <samu24106@gmail.com>
2024-04-24 21:44:25 +0530

    reqq

--- DBMS.py
+++ DBMS.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly as px
+import plotly.express as px
 import pickle
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
